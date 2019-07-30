7f9647a4929cbb8c3ef78961b1a4392c605c1dc2
hudabz <huda_bz@hotmail.com>
2019-07-30 16:00:14 +0300

    comitting react package json

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const expressPort = 3000
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://still-hamlet-58798.herokuapp.com', 
   development: `http://localhost:${expressPort}`
 }
 
