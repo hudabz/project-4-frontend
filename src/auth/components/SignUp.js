1bd51eb4fd911a70e8322972453178ee4f98ed60
hudabz <huda_bz@hotmail.com>
2019-07-30 16:14:45 +0300

    fixed sign up

--- src/auth/components/SignUp.js
+++ src/auth/components/SignUp.js
@@ -40,42 +40,7 @@ class SignUp extends Component {
     const { email, password, passwordConfirmation } = this.state
 
     return (
-      // <form className='auth-form' onSubmit={this.onSignUp}>
-      //   <h3>Sign Up</h3>
-
-      //   <label htmlFor="email">Email</label>
-      //   <input
-      //     required
-      //     name="email"
-      //     value={email}
-      //     type="email"
-      //     placeholder="Email"
-      //     onChange={this.handleChange}
-      //   />
-      //   <label htmlFor="password">Password</label>
-      //   <input
-      //     required
-      //     name="password"
-      //     value={password}
-      //     type="password"
-      //     placeholder="Password"
-      //     onChange={this.handleChange}
-      //   />
-      //   <label htmlFor="passwordConfirmation">Confirm Password</label>
-      //   <input
-      //     required
-      //     name="passwordConfirmation"
-      //     value={passwordConfirmation}
-      //     type="password"
-      //     placeholder="Confirm Password"
-      //     onChange={this.handleChange}
-      //   />
-      //   <button type="submit">Sign Up</button>
-      // </form> 
-
-
-
-<form className='auth-form 'onSubmit={this.onSignIn}>
+<form className='auth-form ' onSubmit={this.onSignUp}>
   <div className='form-group' >
   <h3>Sign Up</h3>
         <label htmlFor="exampleInputEmail1">Email address</label>
